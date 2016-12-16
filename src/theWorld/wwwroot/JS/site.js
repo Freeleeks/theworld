e07c9156ac37a566bf74dfc40d31f41a95f580f9
John <John@CATFISHMCC>
2016-12-16 17:34:31 +0100

    updated js file and started to use jquery

--- src/theWorld/wwwroot/JS/site.js
+++ src/theWorld/wwwroot/JS/site.js
@@ -1,13 +1,35 @@
 ﻿//site.js
-var ele = document.getElementById("username");
-ele.innerHTML = "JOHN DALEY";
 
-var main = document.getElementById("main");
-main.onmouseenter = function () {
-    main.style.backgroundColor = "#888";
-};
 
-main.onmouseleave = function () {
-    main.style.backgroundColor = "";
 
-};
+
+
+
+
+
+(function () {
+
+    /*/var ele = $("#username");
+    ele.text("JOHN DALEY");
+
+    var main =$("#main");
+    main.on("mouseenter", function () {
+        main.style="background-Color:  #888";
+    });
+
+    main.on("mouseleave",
+       function () {  main.style = "";
+       });
+    var menuItems = $("ul.menu li a");
+    menuItems.on("click", function () {
+        var me = $(this);
+        alert(me.text());
+    })*/
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+    $("#sidebarToggle").on("click", function () {
+
+
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+    });
+})();
