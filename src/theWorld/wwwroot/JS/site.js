4e2a90371d84333a87586a5b1e48a2e10d90bf7b
John <John@CATFISHMCC>
2016-12-16 15:23:08 +0100

    Add project files.

---
+++ src/theWorld/wwwroot/JS/site.js
@@ -0,0 +1,13 @@
+﻿//site.js
+var ele = document.getElementById("username");
+ele.innerHTML = "JOHN DALEY";
+
+var main = document.getElementById("main");
+main.onmouseenter = function () {
+    main.style.backgroundColor = "#888";
+};
+
+main.onmouseleave = function () {
+    main.style.backgroundColor = "";
+
+};
